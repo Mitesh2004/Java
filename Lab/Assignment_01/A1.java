bb56c47ccbd50664271ed083531d628d01addc05
Mitesh <122144445+Mitesh2004@users.noreply.github.com>
2024-08-08 20:58:11 +0530

    Add files via upload

---
+++ Lab/Assignment_01/A1.java
@@ -0,0 +1,14 @@
+public class A1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+/* 
+javap java.lang.Object
+javap java.lang.String
+javap java.util.Scanner
+
+javac A1.java
+javap -c A1
+*/
